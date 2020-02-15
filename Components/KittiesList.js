aeb85e32786deb7f62cedb4445abed9e3497891f
kevin <kevin.aubin@protonmail.com>
2020-02-15 20:33:40 +0100

    redux implementation && first test

--- Components/KittiesList.js
+++ Components/KittiesList.js
@@ -1,20 +1,37 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
+import { connect } from 'react-redux'
 
-
-export default class KittiesList extends Component {
+class KittiesList extends React.Component {
 
   state = {
   };
+
+  textUri(){
+        return(
+            <Text>{this.props.imageUri}</Text>
+        )
+  }
 
 
 
     render() {
         return (
-            <View></View>
+            <View>
+                {this.textUri()}
+            </View>
         )
     }
 }
 const styles = StyleSheet.create({
 
 })
+
+
+const mapStateToProps = (state) => {
+    return {
+      imageUri: state.imageUri
+    }
+  }
+  
+  export default connect(mapStateToProps)(KittiesList)
