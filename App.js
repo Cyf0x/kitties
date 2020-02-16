ebae515e790ee1d3e333652d04c622b9d589b512
kevin <kevin.aubin@protonmail.com>
2020-02-16 17:25:46 +0100

    Sqlight
    Addition of sqlight for data persistence

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from 'kitties/Navigation/Navigation';
 import 'react-native-gesture-handler'; 
 import { Provider } from 'react-redux'
 import Store from 'kitties/Reducers/Store/configureStore.js'
+import * as SQLite from "expo-sqlite";
 
-export default function App() {
-  return (
-    <Provider store={Store}>
-      <Navigation  />
-    </Provider>
-  );
+const db = SQLite.openDatabase("db.db");
+
+export default class App extends Component {
+  state = {
+    RecoverIdisEnable: true,
+
+  };
+
+
+
+  componentDidMount() {
+      db.transaction(tx => {
+        tx.executeSql(
+          "create table if not exists cat (cat_id integer primary key not null, cat_biography text,cat_name text,cat_breed text,cat_coat text, cat_image text);"
+        );
+      });
+  }
+
+
+  render() {
+
+    return (
+      <Provider store={Store}>
+        <Navigation  />
+      </Provider>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
