53db923842c914f5415f63ca32a9cd252835deaa
kevin <kevin.aubin@protonmail.com>
2020-02-17 01:11:48 +0100

    Adding an image editing action

--- Reducers/NewKittiesReducer.js
+++ Reducers/NewKittiesReducer.js
@@ -1,4 +1,4 @@
-const initialState = { imageUri: [], myCats:[] }
+const initialState = { imageUri: [], myCats:[], editImage: []}
 
 function reducer(state = initialState, action) {
   let nextState
@@ -10,12 +10,16 @@ function reducer(state = initialState, action) {
           imageUri: [action.value],
         }
         return nextState || state
-
-
     case 'ADD_CAT':
       nextState = {
         ...state,
         myCats: [action.value],
+      }
+      return nextState || state
+    case 'EDIT_IMAGE':
+      nextState = {
+        ...state,
+        editImage: [action.value],
       }
       return nextState || state
     
